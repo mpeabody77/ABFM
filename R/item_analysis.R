f50d4bdd78f26b3832899f01cf3f049d5b97f43f
mpeabody77 <michael.peabody77@gmail.com>
2018-02-08 12:53:44 -0500

    comment out non-live code

--- R/item_analysis.R
+++ R/item_analysis.R
@@ -3,25 +3,25 @@
 #       ANALYSIS
 ##############################
 
-Read Overall.ITM
-Read Overall.DIS
+#Read Overall.ITM
+#Read Overall.DIS
 
 
 
-If N of distractor = 1 or 0, set SE to NA
-Compute by distractor:
-  pvals
-  MSS
-  MSS_SE
+#If N of distractor = 1 or 0, set SE to NA
+#Compute by distractor:
+#  pvals
+#  MSS
+#  MSS_SE
 
-Compute by Item
-  Key
-  Joint SE
-  Admin
-  Mean Bn Flag
-  Rpbim Flag
-  Pvalue Flag
-  Manual Flag
+#Compute by Item
+#  Key
+#  Joint SE
+#  Admin
+#  Mean Bn Flag
+#  Rpbim Flag
+#  Pvalue Flag
+#  Manual Flag
 
 #    make functions for each item analysis the same except that
 #    sports uses a different scaled score formula
@@ -33,17 +33,17 @@ Compute by Item
 ##############################
 
 
-No incorrect Keys
-Correct AN across .DIS
-Correct SEQ across .DIS
+#No incorrect Keys
+#Correct AN across .DIS
+#Correct SEQ across .DIS
 
-Tables:
-  Mean Bn Flags
-  Mean Bn Flag ~ Rpbim Flag
+#Tables:
+#  Mean Bn Flags
+#  Mean Bn Flag ~ Rpbim Flag
 
-Select by Flags and make PDF REPORT (latex or markdown)
+#Select by Flags and make PDF REPORT (latex or markdown)
 
-Item stats Roger excel sheet
+#Item stats Roger excel sheet
 
 
 
